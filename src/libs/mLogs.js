2469ff85a658cec905d584920809dd0bc0bbba16
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-03-11 10:59:42 +0100

    Add check to create logs directory or not

--- src/libs/mLogs.js
+++ src/libs/mLogs.js
@@ -1,11 +1,15 @@
-import { createWriteStream } from 'fs';
+import { createWriteStream, mkdirSync, existsSync } from 'fs';
 import moment from 'moment';
 import mDump from './mDump';
 
 export default class OrmLog {
 
     static print(message) {
-        const filePath = `./logs/${moment().format('YYYYMMDD')}.morn.log`
+        const dirPath = './logs'
+        const filePath = `${dirPath}/${moment().format('YYYYMMDD')}.morn.log`
+
+        if (!existsSync(dirPath))
+            mkdirSync(dirPath)
 
         if (!this.fileStream || this.fileStream.path != filePath)
             this.fileStream = createWriteStream(filePath, { flags: 'a' })
